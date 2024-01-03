bfb80ef60c47d189b2bf56262e1e27a333e33be8
Evgenii Govorushkin <evgenii.govorushkin@telekom.com>
2024-01-03 20:28:05 +0200

    Problem Easy 28

---
+++ src/easy/Easy28FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,33 @@
+package easy;
+/*
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ */
+
+public class Easy28FindTheIndexOfTheFirstOccurrenceInAString {
+    public static int strStr(String haystack, String needle) {
+        int result;
+        if (!haystack.contains(needle)) {
+            result = -1;
+        } else {
+            result = haystack.indexOf(needle);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int result = strStr("hello", "ll");
+        System.out.println(result);
+    }
+}
